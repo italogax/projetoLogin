4dff053882012ab241539369d57aeb14f0909627
Italo Xavier <italogax13@gmail.com>
2024-01-14 00:18:29 -0300

    Tela HTML - Lgin criada; JS para esconder a senha criado; Estilização com CSS na tela de login criada;

---
+++ script.js
@@ -0,0 +1,9 @@
+// Aguarde o carregamento completo do documento antes de manipular o DOM
+document.addEventListener('DOMContentLoaded', function () {
+    const passwordInput = document.getElementById('password');
+
+    // Remova as linhas relacionadas ao span e à função togglePasswordVisibility
+
+    // Garanta que a senha esteja oculta inicialmente (opcional)
+    passwordInput.type = 'password';
+});
